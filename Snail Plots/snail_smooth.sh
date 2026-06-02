c4ff8dc305b356cffaa8490ef10f670abd807088
Mahnoor Nizamani <mahnoor.nizz@gmail.com>
2026-06-02 14:50:26 -0400

    Enhance snail_smooth.sh with comments and instructions
    Added comments for clarity and instructions on using the blobtoolkit.

--- Snail Plots/snail_smooth.sh
+++ Snail Plots/snail_smooth.sh
@@ -1,6 +1,8 @@
 #This was done in command-line
 
 module load apptainer
+#Pull the Docker image as an Apptainer container
+apptainer pull blobtoolkit.sif docker://genomehubs/blobtoolkit:latest
 
 apptainer exec \
     $SCRATCH/blobtoolkit.sif \
@@ -11,3 +13,7 @@ apptainer exec \
         blobtools add --busco /project/def-cottenie/mahnoorn/DFO-Skate-Genomes-Project/results/busco/smooth_busco/run_vertebrata_odb10/full_table.tsv datasets/smoothskate && \
         blobtools view datasets/smoothskate --view snail --plot --out snailplots
     "
+
+#compute canada didnt let it open in an interactive browser
+#--plot creates a .png file of the plot which I copied to my computer using:
+#scp mahnoorn@narval.computecanada.ca:~/scratch/mahnoorn/snailplots/smoothskate/snailplots/smoothskate.snail.png /mnt/c/Users/mahno/OneDrive/Documents
